0cd1ae27cbfe9ebe010020eedffd3cc3324952b6
Rusiru Chapana Jayampathi <rusiruchapana@gmail.com>
2024-07-07 22:42:37 +0530

    create Role jpa entity class.

--- Registration-and-Login-implement-using-springboot-security/src/main/java/com/rusiruchapana/springboot_security/entity/Role.java
+++ Registration-and-Login-implement-using-springboot-security/src/main/java/com/rusiruchapana/springboot_security/entity/Role.java
@@ -7,20 +7,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "users")
+@Table(name = "roles")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
-
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
-    @Column(nullable = false , unique = true)
-    private String email;
-    @Column(nullable = false)
-    private String password;
 }
